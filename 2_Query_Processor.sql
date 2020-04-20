8ba76429efe0658370ebe70dcce7d9ff863c69fe
Filipe <filipepina3@gmail.com>
2020-04-19 20:07:57 -0400

    Update 2_Query_Processor.sql

--- 2_Query_Processor.sql
+++ 2_Query_Processor.sql
@@ -52,4 +52,66 @@
 	Query executer execute the query, it executes the query plan by working through each step it contains and interacting with 
 	Storage Engine to retrieve or modify data.
 */
+
+	/*
 	
+	Database: AdventureWork2012
+	Download: https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15
+	
+	After run each query, check the MESSAGE TAB on Microsft SQL Server Studio
+	*/
+	
+	
+	Query optimizer examples:
+	
+	-- Trivial plan:
+	SELECT BusinessEntityID,
+	       PersonType ModifiedDate
+	FROM Person.Person
+	WHERE BusinessEntityID = 1
+	OPTION(RECOMPILE, QUERYTRACEON 2372);
+	GO
+
+
+	-- Search 0: stage "TP" or transaction processing
+	SELECT PB.BusinessEntityID
+	FROM Person.BusinessEntity PB
+	JOIN Person.Person P 	   ON PB.BusinessEntityID = P.BusinessEntityID
+	JOIN Person.PersonPhone PP ON P.BusinessEntityID  = PP.BusinessEntityID
+	WHERE PB.BusinessEntityID = 200
+	OPTION(RECOMPILE, QUERYTRACEON 2372);
+	GO
+
+	-- Search 1: QuickPlan
+	SELECT PB.BusinessEntityID
+	FROM Person.BusinessEntity PB
+	JOIN Person.Person P
+	ON PB.BusinessEntityID = P.BusinessEntityID
+	JOIN Person.PersonPhone PP
+	ON P.BusinessEntityID = PP.BusinessEntityID 
+	OPTION(RECOMPILE, QUERYTRACEON 2372);
+	GO
+
+
+	-- Search 2: stage "Full" 
+	SELECT PB.BusinessEntityID,
+	       PB.rowguid,
+	       PB.ModifiedDate,
+	       P.BusinessEntityID
+	FROM Person.BusinessEntity PB
+	    JOIN Person.Person P
+		ON PB.BusinessEntityID = P.BusinessEntityID
+	    JOIN Person.PersonPhone PP
+		ON P.BusinessEntityID = PP.BusinessEntityID
+	UNION
+	SELECT PB.BusinessEntityID,
+	       PB.rowguid,
+	       PB.ModifiedDate,
+	       P.BusinessEntityID
+	FROM Person.BusinessEntity PB
+	    JOIN Person.Person P
+		ON PB.BusinessEntityID = P.BusinessEntityID
+	    JOIN Person.PersonPhone PP
+		ON P.BusinessEntityID = PP.BusinessEntityID
+	OPTION(RECOMPILE, QUERYTRACEON 2372);
+	GO
